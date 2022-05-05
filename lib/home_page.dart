64c5cabbda4888eb58ff4213c978899ee2c44834
JMarcos VInicius <viniciusdejesus565@gmail.com>
2022-05-05 11:49:13 -0300

    refactor altera o card de adocao

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -54,8 +54,7 @@ class HomePage extends StatelessWidget {
               height: Get.height * .05,
             ),
             Container(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              height: Get.height * .25,
+              height: Get.height * .15,
               width: double.infinity,
               decoration: BoxDecoration(
                 color: Colors.white,
@@ -64,28 +63,46 @@ class HomePage extends StatelessWidget {
                 ),
               ),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Padding(
-                    padding: const EdgeInsets.all(8.0),
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 3, vertical: 5),
                     child: Container(
-                      width: Get.width * .3,
-                      height: Get.height * .3,
+                      width: Get.width * .32,
+                      height: Get.height * .2,
                       decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(12),
-                          ),
-                          image: DecorationImage(
-                              image: NetworkImage(
-                                  "https://i.pinimg.com/originals/84/48/c8/8448c8666642b94395a81945bf2af015.jpg"))),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(12),
+                        ),
+                        image: DecorationImage(
+                            image: NetworkImage(
+                              "https://i.pinimg.com/originals/84/48/c8/8448c8666642b94395a81945bf2af015.jpg",
+                            ),
+                            fit: BoxFit.cover),
+                      ),
                     ),
                   ),
-                  Expanded(
-                    child: Lottie.network(
-                      'https://assets4.lottiefiles.com/packages/lf20_tr1pjkop.json',
-                      height: Get.height * .26,
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Julieto",
+                          style: TextStyle(color: Colors.black, fontSize: 22),
+                        ),
+                        Text(
+                          "Sertãozinho, SP",
+                          style: TextStyle(color: Colors.grey, fontSize: 18),
+                        ),
+                        Text(
+                          "Macho",
+                          style: TextStyle(color: Colors.grey, fontSize: 18),
+                        ),
+                      ],
                     ),
-                  ),
+                  )
                 ],
               ),
             ),
